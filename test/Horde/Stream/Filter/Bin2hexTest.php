64f1cf412dac2575a3fb3821a108e6a3f0bae740
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-22 08:34:26 +0200

    test: Modernize suite and prepare for src/ PSR-4 version

--- test/Horde/Stream/Filter/Bin2hexTest.php
+++ test/Horde/Stream/Filter/Bin2hexTest.php
@@ -1,46 +1,51 @@
 <?php
 
-/**
- * @category   Horde
- * @package    Stream_Filter
- * @subpackage UnitTests
- */
+declare(strict_types=1);
 
 namespace Horde\Stream\Filter;
 
-use Horde_Test_Case as TestCase;
+use Horde_Stream_Filter_Bin2hex;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
 
-/**
- * @category   Horde
- * @package    Stream_Filter
- * @subpackage UnitTests
- * @coversNothing
- */
+#[CoversClass(Bin2hex::class)]
 class Bin2hexTest extends TestCase
 {
-    public $fp;
-    public $testdata;
+    private string $testdata;
 
-    public function setup(): void
+    /** @var resource */
+    private $fp;
+
+    public function setUp(): void
     {
-        stream_filter_register('horde_bin2hex', 'Horde_Stream_Filter_Bin2hex');
-
         $this->testdata = str_repeat("0123456789ABCDE", 1000);
-
         $this->fp = fopen('php://temp', 'r+');
         fwrite($this->fp, $this->testdata);
     }
 
-    public function testCrc32()
+    public function tearDown(): void
     {
-        $filter = stream_filter_prepend($this->fp, 'horde_bin2hex', STREAM_FILTER_READ);
+        fclose($this->fp);
+    }
 
+    public function testLegacyBin2hex(): void
+    {
+        @stream_filter_register('horde_bin2hex', Horde_Stream_Filter_Bin2hex::class);
+        $filter = stream_filter_prepend($this->fp, 'horde_bin2hex', STREAM_FILTER_READ);
         rewind($this->fp);
 
-        $this->assertEquals(
-            bin2hex($this->testdata),
-            stream_get_contents($this->fp)
-        );
+        $this->assertEquals(bin2hex($this->testdata), stream_get_contents($this->fp));
+
+        stream_filter_remove($filter);
+    }
+
+    public function testModernBin2hex(): void
+    {
+        Bin2hex::register();
+        $filter = stream_filter_prepend($this->fp, Bin2hex::FILTER_NAME, STREAM_FILTER_READ);
+        rewind($this->fp);
+
+        $this->assertEquals(bin2hex($this->testdata), stream_get_contents($this->fp));
 
         stream_filter_remove($filter);
     }
--- test/Horde/Stream/Filter/Crc32Test.php
+++ test/Horde/Stream/Filter/Crc32Test.php
@@ -1,39 +1,37 @@
 <?php
 
-/**
- * @category   Horde
- * @package    Stream_Filter
- * @subpackage UnitTests
- */
+declare(strict_types=1);
 
 namespace Horde\Stream\Filter;
 
-use Horde_Test_Case as TestCase;
+use Horde_Stream_Filter_Crc32;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
 use stdClass;
 
-/**
- * @category   Horde
- * @package    Stream_Filter
- * @subpackage UnitTests
- * @coversNothing
- */
+#[CoversClass(Crc32::class)]
 class Crc32Test extends TestCase
 {
-    public $fp;
-    public $testdata;
+    private string $testdata;
 
-    public function setup(): void
+    /** @var resource */
+    private $fp;
+
+    public function setUp(): void
     {
-        stream_filter_register('horde_crc32', 'Horde_Stream_Filter_Crc32');
-
         $this->testdata = str_repeat("0123456789ABCDE", 1000);
-
         $this->fp = fopen('php://temp', 'r+');
         fwrite($this->fp, $this->testdata);
     }
 
-    public function testCrc32()
+    public function tearDown(): void
     {
+        fclose($this->fp);
+    }
+
+    public function testLegacyCrc32(): void
+    {
+        @stream_filter_register('horde_crc32', Horde_Stream_Filter_Crc32::class);
         $params = new stdClass();
         $filter = stream_filter_prepend($this->fp, 'horde_crc32', STREAM_FILTER_READ, $params);
 
@@ -41,12 +39,24 @@ class Crc32Test extends TestCase
         while (fread($this->fp, 1024)) {
         }
 
-        $this->assertObjectHasAttribute('crc32', $params);
+        $this->assertTrue(property_exists($params, 'crc32'));
+        $this->assertEquals(crc32($this->testdata), $params->crc32);
 
-        $this->assertEquals(
-            crc32($this->testdata),
-            $params->crc32
-        );
+        stream_filter_remove($filter);
+    }
+
+    public function testModernCrc32(): void
+    {
+        Crc32::register();
+        $params = new stdClass();
+        $filter = stream_filter_prepend($this->fp, Crc32::FILTER_NAME, STREAM_FILTER_READ, $params);
+
+        rewind($this->fp);
+        while (fread($this->fp, 1024)) {
+        }
+
+        $this->assertTrue(property_exists($params, 'crc32'));
+        $this->assertEquals(crc32($this->testdata), $params->crc32);
 
         stream_filter_remove($filter);
     }
